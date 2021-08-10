356412c136c2eef38332724b445e2ff6d7d86da7
APF2000 <arthurpfonseca@gmail.com>
2021-08-10 15:34:54 -0300

    Deu bom, mas e UART e ta no docker

--- qemu.sh
+++ qemu.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g script.c -o script.o 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g kbd.c -o kbd.o 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g string.c -o string.o 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g vid.c -o vid.o 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g timer.c -o timer.o 
-arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g keymap.c -o keymap.o 
+arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g test.c -o test.o 
 
-arm-none-eabi-as -c -mcpu=arm926ej-s -g startup.s -o startup.o
-arm-none-eabi-as -c -mcpu=arm926ej-s -g ts.s -o ts.o
+arm-none-eabi-as -c -mcpu=arm926ej-s -g vectors.s -o vectors.o
 
-arm-none-eabi-ld -T t.ld startup.o ts.o script.o kbd.o string.o vid.o timer.o keymap.o -o irq.elf
-arm-none-eabi-objcopy -O binary irq.elf irq.bin
-qemu-system-arm -M versatilepb -m 128M -nographic -s -S -kernel irq.bin
+arm-none-eabi-ld -T test.ld test.o vectors.o -o test.elf
+arm-none-eabi-objcopy -O binary test.elf test.bin
+qemu-system-arm -M versatilepb -m 128M -nographic -s -S -kernel test.bin
+#qemu-system-arm -M versatilepb -serial stdio -kernel test.bin
