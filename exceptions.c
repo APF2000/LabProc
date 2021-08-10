a4927106809ff8990f84883cfd0f2b004097a7ae
APF2000 <arthurpfonseca@gmail.com>
2021-08-10 13:09:31 -0300

    Outros arquivos q n tinham sido commitados

---
+++ exceptions.c
@@ -0,0 +1,7 @@
+/*** exceptions.c file: only show data_abort_handler ***/
+int data_chandler()
+{
+// read MMU registers C5=fault_status, C6=fault_address;
+// print fault_address and fault_status;
+	return 0;
+}
